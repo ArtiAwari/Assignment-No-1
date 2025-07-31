427e1b2f1e5892e8c1de26b2dc64439ee989c864
ArtiAwari <awariarti0407@gmail.com>
2025-07-31 10:51:53 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Division{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int num1,num2;
+System.out.print("Enter first number:");
+num1=s.nextInt();
+System.out.print("Enter second number:");
+num2=s.nextInt();
+System.out.print("Division is:"+(num1/num2));
+}
+}
